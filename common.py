338413d43ac8893516d9f2906c24e0cf835b0779
Tran Quoc Lap <56187517+LapTQ@users.noreply.github.com>
2023-02-11 17:16:15 +0000

    msg

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-
+from tqdm import tqdm
 
 def distribution_from(file_path, undirected):
     assert isinstance(undirected, bool), 'undirected must be boolean'
@@ -25,34 +25,56 @@ def distribution_from(file_path, undirected):
             distribution[s][0].append(d)
             distribution[s][1].append(w)
         
-    return distribution  
-    
+    return distribution 
 
-def contribution_from(distribution, weighted):
+
+def init_PRvalue(distribution):
+    return {s: 1 for s in distribution}
+
+
+def converge_PRvalue(distribution, PRvalue, n_iters, weighted):
     assert isinstance(weighted, bool), 'weighted must be boolean'
 
-    contribution = {}
-    for s in distribution:
-        destinations, weights = distribution[s]
-        for d, w in zip(destinations, weights):
-            
-            if d not in contribution:
-                contribution[d] = ([], [])
-            
-            contribution[d][0].append(s)
-            
-            if not weighted:
-                contrib_coef = 1 / len(destinations)
-            else:
-                contrib_coef = w / sum(weights)
 
-            contribution[d][1].append(contrib_coef)
-    
-    return contribution
+    for _ in tqdm(range(n_iters)):
+        contribution = {}
+        for s in distribution:
+            destinations, weights = distribution[s]
+            for d, w in zip(destinations, weights):
+
+                if d not in contribution:
+                    contribution[d] = ([], [])
+
+                contribution[d][0].append(s)
+
+                if not weighted:
+                    contrib_coef = 1 / len(weights)
+                else:
+                    contrib_coef = w / sum(weights)
+
+                contrib_value = PRvalue[s] * contrib_coef
+
+                contribution[d][1].append(contrib_value)
+        
+        sum_ = 0
+        for d in contribution:
+            contribution[d] = sum(contribution[d][1])
+            sum_ += contribution[d]
+
+        for d in contribution:
+            PRvalue[d] = contribution[d] / sum_
+
+    return PRvalue
 
 
 if __name__ == '__main__':
 
     distribution = distribution_from('popularity_weight.csv', undirected=True)
-    contribution = contribution_from(distribution, weighted=True)
-    print(contribution)
+    PRvalue = init_PRvalue(distribution)
+    PRvalue = converge_PRvalue(distribution, PRvalue, n_iters=10, weighted=False)
+
+    # distribution = distribution_from('test_data.txt', undirected=False)
+    # PRvalue = init_PRvalue(distribution)
+    # PRvalue = converge_PRvalue(distribution, PRvalue, n_iters=10, weighted=False)
+    
+    print(PRvalue)
