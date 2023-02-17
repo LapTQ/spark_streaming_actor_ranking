d40c1c95437422248f255b0a597d6268e0287815
Hung <tonic1307@gmail.com>
2023-02-17 09:47:57 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eb1e774340892949bb6f7c3aa7074ee2b224436603b1176036f62c379075827c
-size 31
+# spark_streaming_actor_ranking
--- wipe.py
+++ wipe.py
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2e3bd50036a1bdab7f55aeca8fae02f1a3bae38f72f51ffa693b2d9f2e848844
-size 259
+from pathlib import Path
+import os
+
+WEIGHTS_DIR = '/user/hadoop/weight'
+
+names = [name for name in os.listdir('.') if name[:2] == 'f_' and name[-4:] == '.csv']
+for name in names:
+    os.system(f'rm {name}')
+    os.system(f'hadoop fs -rm {WEIGHTS_DIR}/{name}')
